69632e3f2d2879c1a945f94762453614552c6a0b
Tifaal <timofey020712@gmail.com>
2026-09-20 14:34:06 +0300

    Delete new_project(module_2)/new_project(module_2) directory

--- new_project(module_2)/new_project(module_2)/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return render_template('index.html')+render_template('index_1.html')
-
-app.run(debug=True)
